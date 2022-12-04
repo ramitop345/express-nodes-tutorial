a58f582ece4a427aeca0f4644f44e303de00b0a2
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2022-12-04 20:06:38 +0100

    using res.json to add json datas to server v1.0.0

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,12 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+const {products} = require('./data')
 
-//used to setup static middleware / make server recognise static files(css, images, ...)
-app.use(express.static('./public'))
-
-// app.get('/',(req,res)=>{
-// res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-// })
-//if we add index.html in public we dont need to use the above function anymore. 
-//the page is loaded automatically once we launch the server
-
-
-app.all('*',(req,res)=>{
-    res.status(404).send('ressource not found')
+app.get('/',(req,res)=>{
+    res.json(products)
 })
 
-app.listen(5000, ()=>{
-    console.log('Server is listenning on port 5000')
+
+app.listen(5000,()=>{
+    console.log('Server is listenint on port 5000')
 })
