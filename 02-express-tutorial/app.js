b757f79dd707b23f5fc170d310b7225f5ea72d0b
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2022-12-04 19:29:35 +0100

    using app.use to automatically add files to server

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,18 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.use(express.static('./public'))
 
 app.get('/',(req,res)=>{
-    console.log('user hit the ressource')
-    res.status(200).send('<h1>Home Page</h1>')
+res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
 })
 
-app.get('./about',(req,res)=>{
-    res.status(200).send('<h1> About Page</h1>')
-})
-
-
-app.get('*',(req,res)=>{
-    res.status(404).send('<h1>ressource not found</h1>')
+app.all('*',(req,res)=>{
+    res.status(404).send('ressource not found')
 })
 
 app.listen(5000, ()=>{
