d1dec91854a98197143cd1649ac3aea648fd9976
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2022-12-04 19:35:52 +0100

    add.use v2.0.0

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+//used to setup static middleware / make server recognise static files(css, images, ...)
 app.use(express.static('./public'))
 
-app.get('/',(req,res)=>{
-res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-})
+// app.get('/',(req,res)=>{
+// res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// })
+//if we add index.html in public we dont need to use the above function anymore. 
+//the page is loaded automatically once we launch the server
+
 
 app.all('*',(req,res)=>{
     res.status(404).send('ressource not found')
