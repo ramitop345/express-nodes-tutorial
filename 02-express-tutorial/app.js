d39ed832fdd6df0ed13b2cc13da94faedaf6b3d2
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2022-12-08 00:10:27 +0100

    using axios for req and res

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,14 +7,15 @@ let {people} = require('./data')
 app.use(express.static('./methods-public'))
 //parser from data
 app.use(express.urlencoded({extended: false}))
-
+app.use(express.json())
+//parse json
 app.get('/api/people', (req,res) => {
     res.status(200).json({succes:true,data:people})
 })
 
-app.post('api/people',(req,res) => {
-    res.status(201).send('Success')
-})
+// app.post('api/people',(req,res) => {
+//     res.status(201).send('Success')
+// })
 
 app.post('/login', (req,res) => {
     const {name} = req.body
@@ -22,6 +23,14 @@ app.post('/login', (req,res) => {
         return res.status(200).send(`Welcome ${name}`)
     }
     res.status(401).send('please provide credentials')
+})
+
+app.post('/api/people', (req,res) => {
+    const {name} = req.body
+    if (!name){
+        return res.status(400).json({success:false,msg:'please provide name value'})
+    }
+    res.status(201).json({success:true, person: name})
 })
 
 app.listen(5000,()=>{
