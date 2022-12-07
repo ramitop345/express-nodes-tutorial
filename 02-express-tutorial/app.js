baa1186d337fd5ca428241e6b1e0b82932f980af
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2022-12-08 00:50:11 +0100

    implementing the app.put method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,8 +7,9 @@ let {people} = require('./data')
 app.use(express.static('./methods-public'))
 //parser from data
 app.use(express.urlencoded({extended: false}))
-app.use(express.json())
 //parse json
+app.use(express.json())
+
 app.get('/api/people', (req,res) => {
     res.status(200).json({succes:true,data:people})
 })
@@ -25,12 +26,25 @@ app.post('/login', (req,res) => {
     res.status(401).send('please provide credentials')
 })
 
+app.post('api/postman/people',(req,res) =>{
+    const {name} = req.body
+    if (!name){
+        return res.status(400).json({success: false, msg: 'please provide name value'})
+    }
+    res.status(201).json({success: true, data:[...people,name]})
+})
+
 app.post('/api/people', (req,res) => {
     const {name} = req.body
     if (!name){
         return res.status(400).json({success:false,msg:'please provide name value'})
     }
     res.status(201).json({success:true, person: name})
+})
+
+app.put('api/people/:id',(req,res) =>{
+    const {id} = req.params
+    const {name} = req.body
 })
 
 app.listen(5000,()=>{
